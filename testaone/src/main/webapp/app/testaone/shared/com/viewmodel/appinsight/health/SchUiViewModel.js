7654554768b4db7a6485d2bdaef9d02fc012b245
shweta.zagade@algorhythm.co.in <applifire@836552917ff5>
2016-06-03 13:58:18 +0000

    File deleting

--- testaone/src/main/webapp/app/testaone/shared/com/viewmodel/appinsight/health/SchUiViewModel.js
+++
@@ -1,5 +0,0 @@
-Ext.define('Testaone.testaone.shared.com.viewmodel.appinsight.health.SchUiViewModel', {
-     'extend': 'Ext.app.ViewModel',
-     'alias': 'viewmodel.SchUiViewModel',
-     'model': 'SchUiModel'
-});
