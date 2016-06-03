111e40b41bc6e43774c4857b74a920c063ca9532
shweta.zagade@algorhythm.co.in <applifire@836552917ff5>
2016-06-03 13:57:26 +0000

    File deleting

--- testaone/src/main/webapp/app/testaone/shared/com/viewmodel/appinsight/health/EmpViewModel.js
+++
@@ -1,6 +0,0 @@
-Ext.define('Testaone.testaone.shared.com.viewmodel.appinsight.health.EmpViewModel', {
-     "extend": "Ext.app.ViewModel",
-     "alias": "viewmodel.EmpViewModel",
-     "model": "EmpModel",
-     "data": {}
-});
