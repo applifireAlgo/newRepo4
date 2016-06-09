7f375d073aaaff91f47fbfd5e6a89466105b211b
shweta.zagade1209@gmail.com <applifire@e234a60f0bb5>
2016-06-09 14:02:08 +0000

    File deleting

--- testlic/src/main/webapp/app/testlic/shared/com/viewmodel/testbc/testing/AuiViewModel.js
+++
@@ -1,5 +0,0 @@
-Ext.define('Testlic.testlic.shared.com.viewmodel.testbc.testing.AuiViewModel', {
-     'extend': 'Ext.app.ViewModel',
-     'alias': 'viewmodel.AuiViewModel',
-     'model': 'AuiModel'
-});
--- testlic/src/main/webapp/app/testlic/shared/com/viewmodel/testbc/testing/NameTestViewModel.js
+++
@@ -1,6 +0,0 @@
-Ext.define('Testlic.testlic.shared.com.viewmodel.testbc.testing.NameTestViewModel', {
-     "extend": "Ext.app.ViewModel",
-     "alias": "viewmodel.NameTestViewModel",
-     "model": "NameTestModel",
-     "data": {}
-});
